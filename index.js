c174494597a35d9f48872eae1fbe540ebf258d9c
maryamxebrahim <maryamxebrahim@gmail.com>
2018-11-19 09:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var instruments = ["Piano","Guitar","Drums"];
 
 Function theBeatlesPlay (musicians,instruments) {
   beatleArray = [];
-  for (let i=0; i<musicians.length,i++) {
+  for (let i=0; i<musicians.length;i++) {
     beatleArray.push(musicians[i] + " plays " + instruments[i]);
   }
   return beatleArray;
