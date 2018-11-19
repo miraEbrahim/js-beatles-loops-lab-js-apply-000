b93494483b9973fa709905eb83d7e672b85bf81a
maryamxebrahim <maryamxebrahim@gmail.com>
2018-11-19 09:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // add solution here
-var musicians = ["B Young", "Geko", "Alec Benjamin"]
-var instruments = ["Piano","Guitar",""]
+var musicians = ["B Young", "Geko", "Alec Benjamin"];
+var instruments = ["Piano","Guitar","Drums"];
