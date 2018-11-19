c12d3cc943e8f83271e7d910b14ab100c8c655f6
maryamxebrahim <maryamxebrahim@gmail.com>
2018-11-19 09:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // add solution here
 var musicians = ["B Young", "Geko", "Alec Benjamin"];
 var instruments = ["Piano","Guitar","Drums"];
+
+Function theBeatlesPlay (musicians,instruments) {
+  beatleArray = []
+  for (i)
+}
